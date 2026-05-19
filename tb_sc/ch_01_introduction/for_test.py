204f90acd21495edd381c7f2a79dc4fe66ddaf65
Frode Næsje <frode.nasje@gmail.com>
2026-05-19 10:49:47 +0200

    Remove package experiment script from tb_sc and ignore similar files

--- tb_sc/ch_01_introduction/for_test.py
+++
@@ -1,9 +0,0 @@
-print(f"{'Day':^15}{'Temperature':^10}")
-print(f"{'Monday':^15}{20.5:^10}")
-print(f"{'Tuesday':^15}{18.2:^10}")
-print(f"{'Wednesday':^15}{22.8:^10}")
-
-print(f"{'Day':15}{'Temperature':10}")
-print(f"{'Monday':15}{20.5:10}")
-print(f"{'Tuesday':15}{18.2:10}")
-print(f"{'Wednesday':15}{22.8:10}")
